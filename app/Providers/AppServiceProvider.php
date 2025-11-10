3cfb051a2d82a6a2ece94b7f4b48354a9bbaccfc
unknown <midfaicmi@gmail.com>
2025-11-10 18:46:20 +0800

    debugging CI: ignore the missing manifest during tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Livewire\Livewire;
 use App\Http\Livewire\EditArticleModal;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         // Livewire::component('edit-article-modal', EditArticleModal::class);
+        if (app()->environment('testing')) {
+            Vite::useHotFile(null);
+            Vite::useBuildDirectory('fake'); // prevent looking for /public/build
+        }
     }
 }
